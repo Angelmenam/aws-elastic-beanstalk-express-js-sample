47d809b5734e7fc490bbb331e9635f0eec354c33
Angel <angel90909@hotmail.com>
2024-11-25 04:12:44 -0600

    Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('The true end is not the beggining'));
+app.get('/', (req, res) => res.send('And now I stare at the abyss, but not even the abyss would stare back at me.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
